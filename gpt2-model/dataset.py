1e0aad6447af580c789d067f2707d680185c2988
notdiff <sidorovvladimir1998@gmail.com>
2025-08-02 19:17:29 +0300

    feat: dataloader

---
+++ gpt2-model/dataset.py
@@ -0,0 +1,34 @@
+import tiktoken
+import torch
+
+class DataLoaderLite:
+    def __init__(self, B: int, T: int):
+        self.B = B
+        self.T = T
+
+        with open("input.txt") as f:
+            text = f.read()
+
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) / (B*T)}")
+
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+
+        buf = self.tokens[self.current_position:self.current_position+B*T+1]
+        
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+
+        self.current_position += B*T
+
+        if self.current_position + (B*T + 1) > len(self.tokens):
+            self.current_position = 0
+
+        return x, y
